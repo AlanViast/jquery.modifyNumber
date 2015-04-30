46d65e84bedb2e20fbcfa2b93285833921446647
AlanViast <AlanViast@gmail.com>
2015-04-30 13:21:19 +0800

    添加oldValue机制

--- js/jquery.modifyNumber.js
+++ js/jquery.modifyNumber.js
@@ -17,7 +17,7 @@
 
     $.fn.modifyNumber = function(tempOption){
         var option            = $.extend(true, defaultOption, tempOption),
-              oldValue        = option.defaultValue,
+              defaultValue  = option.defaultValue,
               defaultMax    = option.max,
               defaultMin     = option.min,
               $this              = $(this);
@@ -30,8 +30,23 @@
 
         $this.find(option.inputClass).change(function(event) {
             event.preventDefault();
+            console.dir(event);
             $(this).trigger('mn.change');
         });
+
+        $this.find(option.inputClass).focus(function(event) {
+            event.preventDefault();
+            $(this).data('oldValue', $(this).val());
+        });
+
+         $this.find(option.inputClass).keydown(function(event) {
+                if ($.inArray(event.keyCode, [46, 8, 9, 27, 13, 190]) !== -1 || ( event.keyCode == 65 && event.ctrlKey === true ) ||  ( event.keyCode >= 35 && event.keyCode <= 39 )) {
+                        return;
+                }
+                if ((event.shiftKey || (event.keyCode < 48 || event.keyCode > 57)) && (event.keyCode < 96 || event.keyCode > 105)) {
+                   event.preventDefault();
+                }
+         });
 
         $this.on('mn.minus', function(event){
             event.preventDefault();
@@ -46,7 +61,7 @@
         $this.on('mn.plus', function(event){
             event.preventDefault();
             var $inputNumber = $(this).find('input');
-            var value = parseInt($inputNumber.val()) + option.step;
+            var value = parseInt($inputNumber.val());
             if (!isNaN(value)) {
                 $inputNumber.val(value + option.step);
             }
@@ -55,17 +70,17 @@
 
         $this.on('mn.change', function(event){
             event.preventDefault();
-            var $inputNumber = $(this).find(option.inputClass);
-            var value = parseInt($inputNumber.val());
-
-            var max = parseInt($inputNumber.data('mn-max')) || defaultMax;
-            var min  = parseInt($inputNumber.data('mn-min')) || defaultMin;
+            var $inputNumber = $(this).find(option.inputClass),
+                   value = parseInt($inputNumber.val()),
+                   max = parseInt($inputNumber.data('mn-max')) || defaultMax,
+                   min  = parseInt($inputNumber.data('mn-min')) || defaultMin,
+                   oldValue = $inputNumber.data('oldValue') || defaultValue;
 
             if(isNaN(value) || (max < value || min > value)){
                 $inputNumber.val(oldValue);
             } else {
                 $inputNumber.val(value);
-                oldValue = value;
+                $inputNumber.data('oldValue', value);
             }
             $(this).trigger('mn.valueChange');
         });
